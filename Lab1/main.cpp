369d3313ee82fd87025a99c04e95bcd9e1f0037e
sinnlaw <chinglonglaw@gmail.com>
2014-09-23 10:58:20 +0100

    Lab1

---
+++ Lab1/main.cpp
@@ -0,0 +1,22 @@
+//Head file
+#include <istream>
+
+//Golbal variables
+
+//Clean Up function
+void CleanUp(){
+
+}
+
+//Global function
+void InitWindow(int width, int height, bool fullscreen){
+
+
+
+}
+
+//Main Method - Entry Point
+int main(int argc, char* arg[]){
+	return 0;
+}
+
